59be4159d7b2e43421d73ce7b476f59dc32a1a7e
scovert3197 <samuelcovert@samuels-mbp.attlocal.net>
2017-01-28 23:59:13 -0800

    inclusion of user models

--- routes.py
+++ routes.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/covana'
+
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
